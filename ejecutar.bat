0a79293ce5e82a96cd0137db89def314588fb49b
generiz <39973192+generiz@users.noreply.github.com>
2023-04-14 07:18:10 -0600

    Agrega verificación de permisos de administrador
    Añade un código en ejecutar.bat para verificar si el script se está ejecutando con permisos de administrador. Si no se ejecuta como administrador, muestra un mensaje de error y termina la ejecución del script

--- ejecutar.bat
+++ ejecutar.bat
@@ -1,5 +1,14 @@
 @echo off
+:: Verificar si se ejecuta como administrador
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Ejecutando como administrador.
+) else (
+    echo Error: Este script requiere permisos de administrador.
+    echo Por favor, ejecute este script como administrador.
+    pause
+    exit
+)
 cd /d %~dp0
 powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& { "%~dp0\datos\solucion.ps1 }"
-
 
