1ff27f4c46869f9c6fe6df54592647d8a2222cf6
generiz <39973192+generiz@users.noreply.github.com>
2023-09-07 20:08:53 -0600

    Detección y manejo de monitores externos en el script tarea.ps1
    Detección de Monitores Externos: Se implementó una lógica para detectar la presencia de monitores externos conectados al sistema.
    
    Deshabilitar Virtual Display: Si se detecta un monitor externo, el script ahora deshabilita automáticamente el Virtual Display para evitar conflictos.
    
    Optimización de la Lógica: Se reorganizó la secuencia de instalación y desinstalación del Virtual Display para ser más eficiente, asegurando que sólo se realicen las operaciones necesarias según el contexto.

--- datos/carpeta/tarea.ps1
+++ datos/carpeta/tarea.ps1
@@ -1,16 +1,32 @@
+$generizPath = "c:\generiz"
+Set-Location $generizPath
+
 # Verifica si el dispositivo de visualización virtual existe
 $virtualDisplay = Get-WmiObject -Query "SELECT * FROM Win32_PnPEntity WHERE PNPClass='DISPLAY' AND Name LIKE '%Virtual Display%'"
 
-if ($virtualDisplay) {
-    Write-Host "Virtual Display encontrado. Desinstalando..."
-    Set-Location "c:\generiz\"
-    .\deviceinstaller64.exe enableidd 0
-    .\deviceinstaller64.exe remove usbmmidd.inf usbmmidd
+# Verificar monitores adicionales
+$additionalMonitors = Get-WmiObject -Query "SELECT * FROM Win32_PnPEntity WHERE PNPClass='DISPLAY' AND Name NOT LIKE '%Virtual Display%'"
+
+if ($additionalMonitors) {
+    Write-Host "Detectado monitor adicional."
+
+    if ($virtualDisplay) {
+        Write-Host "Virtual Display encontrado. Deshabilitando..."
+        .\deviceinstaller64.exe enableidd 0
+        .\deviceinstaller64.exe remove usbmmidd.inf usbmmidd
+        Write-Host "Virtual Display deshabilitado."
+    } else {
+        Write-Host "No hay Virtual Display para deshabilitar."
+    }
+} else {
+    if ($virtualDisplay) {
+        Write-Host "Virtual Display encontrado. Desinstalando..."
+        .\deviceinstaller64.exe enableidd 0
+        .\deviceinstaller64.exe remove usbmmidd.inf usbmmidd
+    }
+
+    Write-Host "Instalando Virtual Display..."
+    .\deviceinstaller64.exe install usbmmidd.inf usbmmidd
+    .\deviceinstaller64.exe enableidd 1
+    Write-Host "Virtual Display reinstalado correctamente."
 }
-
-Write-Host "Instalando Virtual Display..."
-Set-Location "c:\generiz\"
-.\deviceinstaller64.exe install usbmmidd.inf usbmmidd
-.\deviceinstaller64.exe enableidd 1
-
-Write-Host "Virtual Display reinstalado correctamente."
